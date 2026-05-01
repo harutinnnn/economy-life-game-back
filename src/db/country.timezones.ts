48e688d0ccd0ca36a66e108351f7fb7b804d76d3
harut <harutinnnn@gmail.com>
2026-05-01 22:17:17 +0400

    First

--- src/db/country.timezones.ts
+++ src/db/country.timezones.ts
@@ -6,38 +6,44 @@ import {countries, timezones} from "./schema";
 
 const fileName = "countries-timezones.json";
 
-(async () => {
+type CountryTimezoneInput = {
+    name: string;
+    code: string;
+    capital?: string;
+    timezones?: Array<{
+        timezone: string;
+        utcOffset?: string;
+    }>;
+};
 
+async function seedCountriesAndTimezones() {
+    const filePath = path.join(__dirname, fileName);
+    const file = fs.readFileSync(filePath, "utf-8");
+    const countryList: CountryTimezoneInput[] = JSON.parse(file);
 
-    const file = fs.readFileSync(path.join(__dirname, fileName));
-    const countryList = JSON.parse(file);
-    countryList.map(async ele => {
+    for (const ele of countryList) {
+        const [tmpCountry] = await db.insert(countries).values({
+            name: ele.name,
+            code: ele.code,
+            capital: ele.capital || "",
+        });
 
-
-        const [tmpCountry] = await db.insert(countries).values(
-            {
-                name: ele.name,
-                code: ele.code,
-                capital: ele.capital || "",
-            }
-        );
-
-        if (tmpCountry.insertId && ele.timezones.length > 0) {
-
-            ele.timezones.map(async tEle => {
-
-                await db.insert(timezones).values(
-                    {
-                        countryId: tmpCountry.insertId,
-                        timezoneName: tEle.timezone,
-                        utcOffset: tEle.utcOffset || "",
-                    }
-                );
-            })
-
+        const insertId = tmpCountry?.insertId;
+        if (!insertId || !ele.timezones?.length) {
+            continue;
         }
 
+        for (const tEle of ele.timezones) {
+            await db.insert(timezones).values({
+                countryId: insertId,
+                timezoneName: tEle.timezone,
+                utcOffset: tEle.utcOffset || "",
+            });
+        }
+    }
+}
 
-    })
-
-})()
+seedCountriesAndTimezones().catch((error) => {
+    console.error("Failed to seed countries and timezones:", error);
+    process.exit(1);
+});
